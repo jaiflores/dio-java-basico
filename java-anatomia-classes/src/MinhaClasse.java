70fc12b6061b46ad16cc4902c5d233fdd7f60e58
jaiflores <jaianny.vivian@gmail.com>
2024-07-10 14:42:06 -0300

    Estudo da anatomia das classes com a DIO me

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,16 @@
 public class MinhaClasse {
 
-    public static void main(String[] args) {
-        System.out.print("Olá, Mundo!");
+    public static void main (String [] args) {
+
+        String primeiroNome = "Jaianny";
+        String segundoNome = "Vivian";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
     
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return  primeiroNome.concat(" ").concat(segundoNome);
+}
+
 }
